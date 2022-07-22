e1caf2460c8a1af06064f545ec81940b1a174bf6
igoodwinVolly <igoodwin@myvolly.com>
2022-07-22 14:58:50 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,8 @@ do
       then
         # Merge changes from related source branch to related destination branch
         echo "Merging changes from ${source} to ${destination}..."
-        PR=$(gh pr create --head $source --base $destination --title "Merge changes from ${source} to ${destination}")
+        echo "gh pr create --base $destination --head $source --fill"
+        PR=$(gh pr create --base $destination --head $source --fill)
         echo $PR
       fi
     fi
