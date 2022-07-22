e3afc1ca8102224701e3c91cc32b9f138940d96d
igoodwinVolly <igoodwin@myvolly.com>
2022-07-22 15:39:12 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,38 +33,47 @@ echo "${INPUT_DESTINATIONS}"
 
 # gh pr create --base $line --head main; done
 
+# Returns 0 if branches are the same, 1 if else
+diff_branches () {
+  # Check if branches are the same
+  # $1 source
+  # $2 destination
+  if [ "$(git rev-parse --revs-only "$1")" = "$(git rev-parse --revs-only "$2")" ];
+  then
+    echo "Source and destination branches are the same"
+    return 0
+  else
+    # Do not proceed if there are no file differences, this avoids PRs with just a merge commit and no content
+    LINES_CHANGED=$(git diff --name-only "$2" "$1" -- | wc -l | awk '{print $1}')
+    if [[ "$LINES_CHANGED" = "0" ]] && [[ ! "$INPUT_PR_ALLOW_EMPTY" ==  "true" ]];
+    then
+      echo "No file changes detected between source and destination branches."
+      return 0
+    fi
+  fi
+  return 1
+}
+
 for destination in $INPUT_DESTINATIONS
 do
   for source in $INPUT_SOURCES
   do
-    if [ $source == "main" ];
-    then # Merge changes from main to destination branch
-      echo "Merging changes from ${source} to ${destination}..."
-      echo "gh pr create --base $destination --head $source --fill"
-      PR=$(gh pr create --base $destination --head $source --fill)
-      echo $PR
-    else
-      SRC=$(echo $source | sed "s/$INPUT_SOURCE_REGEX//")
-      DST=$(echo $destination | sed "s/$INPUT_DESTINATION_REGEX//")
-      if [[ $SRC == $DST ]];
+    if [ diff_branches "$source" "$destination" ];
+    then
+      if [ $source == "main" ];
       then
-        # Check if branches are the same
-        if [ "$(git rev-parse --revs-only "$source")" = "$(git rev-parse --revs-only "$destination")" ];
-        then
-          echo "Source and destination branches are the same"
-        else
-          # Do not proceed if there are no file differences, this avoids PRs with just a merge commit and no content
-          LINES_CHANGED=$(git diff --name-only "$destination" "$source" -- | wc -l | awk '{print $1}')
-          if [[ "$LINES_CHANGED" = "0" ]] && [[ ! "$INPUT_PR_ALLOW_EMPTY" ==  "true" ]];
-          then
-            echo "No file changes detected between source and destination branches."
-          else
-            # Merge changes from related source branch to related destination branch
-            echo "Merging changes from ${source} to ${destination}..."
-            echo "gh pr create --base $destination --head $source --fill"
-            PR=$(gh pr create --base $destination --head $source --fill)
-            echo $PR
-          fi
+        # Merge changes from main to destination branch
+        echo "Merging changes from ${source} to ${destination}..."
+        PR=$(gh pr create --base $destination --head $source --title "Merge changes from $source to $destination" --body "Opened by bot")
+        echo $PR
+      else
+        SRC=$(echo $source | sed "s/$INPUT_SOURCE_REGEX//")
+        DST=$(echo $destination | sed "s/$INPUT_DESTINATION_REGEX//")
+        if [[ $SRC == $DST ]];
+          # Merge changes from related source branch to related destination branch
+          echo "Merging changes from ${source} to ${destination}..."
+          PR=$(gh pr create --base $destination --head $source --title "Merge changes from $source to $destination" --body "Opened by bot")
+          echo $PR
         fi
       fi
     fi
